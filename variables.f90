4f1f459286d9ad4b3e0de9d4902d3f9502244720
FlaneTern <hertgeathertmc@gmail.com>
2024-08-04 13:26:11 +0700

    variables continuation

--- variables.f90
+++ variables.f90
@@ -7,6 +7,13 @@ program variables
   character :: initial
   logical :: isOkay
 
+  real :: radius
+  real :: height
+  real :: area
+  real :: volume
+
+  integer :: age
+  
   amount = 10
   pi = 3.1415927
   frequency = (1.0, -0.5)
@@ -18,4 +25,34 @@ program variables
   print *, 'The value of frequency (complex) is: ', frequency
   print *, 'The value of initial (character) is: ', initial
   print *, 'The value of isOkay (logical) is: ', isOkay
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  
+
+  print *, 'Please enter your age: '
+  read(*,*) age
+  print *, 'Your age is: ', age
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  
+
+
+  print *, 'Enter cylinder base radius:'
+  read(*,*) radius
+
+  print *, 'Enter cylinder height:'
+  read(*,*) height
+
+  area = pi * radius**2
+  volume = area * height
+
+  print *, 'Cylinder radius is: ', radius
+  print *, 'Cylinder height is: ', height
+  print *, 'Cylinder base area is: ', area
+  print *, 'Cylinder volume is: ', volume
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+  
+  
 end program variables
